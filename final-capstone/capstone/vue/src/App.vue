a993973709298127f8987d6095509b71b3b124c3
Andrew Morrison <am4188@gmail.com>
2023-05-31 12:31:39 -0400

    v4

--- final-capstone/capstone/vue/src/App.vue
+++ final-capstone/capstone/vue/src/App.vue
@@ -1,34 +1,23 @@
 <template>
   <div id="app">
     <div id="nav">
-      <!-- <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp; -->
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp; -->
+       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link> -->
     </div>
-    <div class="router"><router-view /></div>
+   
+    <div class="router"> <router-view /></div>
   </div>
 </template>
 
 <style>
-<<<<<<< HEAD
-#app {
+/* #app {
   height: 2vh;
 }
 
 .router {
   height: 97vh;
-}
-
-
+} */
 
 </style>
 
 
-=======
-
-
-#app {
-  background-color: rgb(110, 226, 228);
-  height:100vh;
-}
-</style>
->>>>>>> 742396f974d9521be2a155ad1ce4df2db79ea048
