1514f3196e2c3fc87ce1e78746b9e538dc8656cd
Raul Haro <raulharo99@gmail.com>
2023-05-31 17:05:15 -0400

    added SQL to create a profiles table in the database and added a test profile account to the database

--- final-capstone/capstone/java/database/data.sql
+++ final-capstone/capstone/java/database/data.sql
@@ -3,4 +3,7 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO profiles (user_id, first_name, last_name, age, height, current_weight, goal_weight, calorie_limit, mindful_goal)
+VALUES (1, 'Test', 'User', 25, 72, 180.74, 170.00, 1500, 10);
+
 COMMIT TRANSACTION;
--- final-capstone/capstone/java/database/schema.sql
+++ final-capstone/capstone/java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, profiles CASCADE;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -8,6 +8,21 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE profiles (
+    profile_id serial,
+    user_id int,
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    age int NOT NULL,
+    height int NOT NULL,
+    current_weight numeric(6,2) NOT NULL,
+    goal_weight numeric(6,2) NOT NULL,
+    calorie_limit int NOT NULL,
+    mindful_goal int NOT NULL,
+    CONSTRAINT PK_profile_id PRIMARY KEY (profile_id),
+    CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 
 COMMIT TRANSACTION;
