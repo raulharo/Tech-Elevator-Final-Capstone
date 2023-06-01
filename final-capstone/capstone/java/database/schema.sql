96d84772cd492c300b34b01f856f3b39bb7aae2b
Raul Haro <raulharo99@gmail.com>
2023-06-01 17:43:11 -0400

    added new tables to schema create script in java database folder

--- final-capstone/capstone/java/database/schema.sql
+++ final-capstone/capstone/java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users, profiles CASCADE;
+DROP TABLE IF EXISTS users, profiles, foods, meal_history, mindful_history CASCADE;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -22,6 +22,37 @@ CREATE TABLE profiles (
     calorie_limit int NOT NULL,
     mindful_goal int NOT NULL,
     CONSTRAINT PK_profile_id PRIMARY KEY (profile_id),
+    CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+CREATE TABLE foods (
+    food_id serial,
+    food_name varchar(50) NOT NULL,
+    calories int NOT NULL,
+    serving_size varchar(50) NOT NULL,
+    CONSTRAINT PK_food_id PRIMARY KEY (food_id)
+);
+
+CREATE TABLE meal_history (
+    meal_history_id serial,
+    food_id int NOT NULL,
+    user_id int NOT NULL,
+    meal_date date NOT NULL,
+    type varchar(50) NOT NULL,
+    number_of_servings numeric(2,2) NOT NULL,
+    total_calories int NOT NULL,
+    CONSTRAINT PK_meal_history_id PRIMARY KEY (meal_history_id),
+    CONSTRAINT FK_food_id FOREIGN KEY (food_id) REFERENCES foods (food_id),
+    CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+CREATE TABLE mindful_history (
+    mindful_id serial,
+    activity varchar(50) NOT NULL,
+    user_id int NOT NULL,
+    mindful_date date NOT NULL,
+    length_minutes int NOT NULL,
+    CONSTRAINT PK_mindful_id PRIMARY KEY (mindful_id),
     CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 
